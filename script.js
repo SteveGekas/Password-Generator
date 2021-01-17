ea7e7687e193e15c20b4ae06d9bff7a4f113b46d
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-01-17 10:38:57 -0500

    fixed errors in showing password

--- script.js
+++ script.js
@@ -4,71 +4,79 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var symbols = "`~!@#$%^&*()_+=-[]\|}{;:,<.>/?";
 var possibleCharacters = ''
-//var finalPwd = '';
+
 
 
 
 function generatePassword() {
-  var lengthConfirm = prompt("Please choose between 8 and 128 characters for your password.");
+  var finalPwd = '';
+  var lengthConfirm = prompt("Please choose between 8 and 128 characters to make your password.");
 
   if (lengthConfirm < 8 || lengthConfirm > 128) {
     alert("Password must be BETWEEN 8 and 128 characters.")
+    console.log(lengthConfirm);
+  }
+
+  else {
+    var lowerConfirm = confirm("Would you like it to contain lower case letters?");
+
+
+    if (lowerConfirm) {
+      possibleCharacters += lowerCase;
+      console.log(possibleCharacters);
+    }
+
+    var upperConfirm = confirm("Would you like it to contain upper case letters?");
+
+    if (upperConfirm) {
+      possibleCharacters += upperCase;
+      console.log(possibleCharacters);
+    }
+    var numberConfirm = confirm("Would you like it to contain numbers?");
+
+    if (numberConfirm) {
+      possibleCharacters += numbers;
+      console.log(possibleCharacters);
+    }
+
+    var symbolConfirm = confirm("Would you like it to contain symbols?");
+
+    if (symbolConfirm) {
+      possibleCharacters += symbols;
+      console.log(possibleCharacters);
+
+    }
+
+  
+  if (lowerConfirm === false && upperConfirm === false && numberConfirm === false && symbolConfirm === false) {
+    alert("At least one character set MUST be chosen.")
     //generatePassword()
   }
 
-  var lowerConfirm = confirm("Would you like it to contain lower case letters?");
+  for (var i = 0; i < lengthConfirm; i++) {
 
+    var index = Math.floor(Math.random() * possibleCharacters.length);
 
-  if (lowerConfirm) {
-    possibleCharacters += lowerCase;
-    console.log(possibleCharacters)
-  }
-
-  var upperConfirm = confirm("Would you like it to contain upper case letters?");
-
-  if (upperConfirm) {
-    possibleCharacters += upperCase;
-    console.log(possibleCharacters)
-  }
-  var numberConfirm = confirm("Would you like it to contain numbers?");
-
-  if (numberConfirm) {
-    possibleCharacters += numbers;
-    console.log(possibleCharacters)
-  }
-
-  var symbolConfirm = confirm("Would you like it to contain symbols?");
-
-  if (symbolConfirm) {
-    possibleCharacters += symbols;
-    console.log(possibleCharacters);
+    finalPwd += possibleCharacters[index];
 
   }
-
-
-  if (lowerConfirm === false && upperConfirm === false && numberConfirm === false && symbolConfirm === false) {
-    alert("At least one character set MUST be chosen.")
-    generatePassword()
-  }
-  //possibleCharacters = 
-
-  for (var i = 0; i < possibleCharacters.length; i++) {
-     finalPwd =  document.getElementsByName(possibleCharacters);
-  }
-
-  
+  return finalPwd;
 }
-  // Assignment Code
-  var generateBtn = document.querySelector("#generate");
+
+}
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword(possibleCharacters);
-    var passwordText = document.querySelector('#password');
-  console.log(finalPwd);
-    passwordText.value = password;
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener('click', writePassword);
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
+
+
